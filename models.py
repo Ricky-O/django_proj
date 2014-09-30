509469815acab12cebd4059697e63b0329c010d9
Ricky Orndorff <richard.orndorff2012@gmail.com>
2014-09-29 20:30:27 -0400

    2nd commit

---
+++ models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=100)
+    access_date = models.DateTimeField('date accessed')
+    access_url = models.CharField(max_length=200)
+
+class Access(models.Model):
+    user = models.ForeignKey(User)
+    user_text = models.CharField(max_length=200)
+    hits = models.IntegerField(default=0)
+
+
+
